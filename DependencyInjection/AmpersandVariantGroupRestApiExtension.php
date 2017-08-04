3713e3002459248e6a72c7bc05e1e2550220f4c8
Cristian Quiroz <cris@qrz.io>
2017-08-04 15:17:57 +0100

    Load the right yml config

--- DependencyInjection/AmpersandVariantGroupRestApiExtension.php
+++ DependencyInjection/AmpersandVariantGroupRestApiExtension.php
@@ -12,8 +12,7 @@ class AmpersandVariantGroupRestApiExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('steps.yml');
-        $loader->load('processors.yml');
-        $loader->load('writers.yml');
+
+        $loader->load('controllers.yml');
     }
 }
